684661f8ebf0c40213fc603f6c0d74bded182939
Razzoy <1081892@ucn.dk>
2024-08-14 11:14:52 +0200

    About addition v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
-import "./src/assets/styles/style.css";
+import "./assets/styles/style.css";
+
+import { About } from "./assets/components/About/About"
 
 function App() {
-  const [count, setCount] = useState(0);
+  
+  const aboutHT = 'Under the Moon - New Style' 
+  const aboutPT = 'Lorem ipsum dolor sit amet consectetur. Orci elementum ullamcorper feugiat vitae faucibus. Sodales neque quisque sodales etiam tortor ut praesent. Id luctus tincidunt aenean molestie pellentesque. Viverra sit aliquam faucibus volutpat lectus sed eu euismod. Suspendisse augue ipsum pellentesque sagittis nunc. Maecenas mattis neque orci bibendum quam faucibus amet platea congue. Amet condimentum faucibus ultrices nunc varius nisl viverra vivamus amet. Habitant dictum fermentum vitae proin ut. Eu faucibus molestie lacus porttitor sed pellentesque turpis in. Aliquam'
+  const aboutBT = 'Læs mere'
 
   return (
     <>
-      <p>Halløj verden</p>
+      <About aboutHeaderText = {aboutHT} aboutParagraphText = {aboutPT} aboutButtonText = {aboutBT}/>
     </>
   );
 }
