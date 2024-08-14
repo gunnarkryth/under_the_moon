17d8e08243528baecc6a12d259e96ed768bafc7d
Razzoy <1081892@ucn.dk>
2024-08-14 13:31:55 +0200

    Preview Section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
-import "./src/assets/styles/style.css";
+// import "./src/assets/styles/style.css";
+
+import { Preview } from "./assets/components/Preview/Preview";
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const previewHT1 = 'One 2 One'
+  const previewHT2 = 'Loyalty Free'
+  const previewT = 'Lorem ipsum dolor sit amet consectetur. Nunc pellentesque senectus aliquam ipsum. Sed mauris posuere odio nisl in. Massa vitae faucibus imperdiet facilisi pulvinar maecenas platea purus pulvinar. Lacinia imperdiet nisi nunc vel ut convallis a est scelerisque'
+  const previewB1 = 'Book tid'
+  const previewB2 = 'Læs mere'
+  
 
   return (
     <>
-      <p>Halløj verden</p>
+      <Preview previewHeaderText1 = {previewHT1} previewHeaderText2 = {previewHT2} previewText = {previewT} previewButton1 = {previewB1} previewButton2 = {previewB2} />
     </>
   );
 }
---
+++ src/assets/components/Preview/Preview.jsx
@@ -0,0 +1,24 @@
+import style from '../Preview/Preview.module.scss'
+import preview1 from '../../images/preview1.png'
+import preview2 from '../../images/preview2.png'
+
+export function Preview(props) {
+    return (
+        <>
+            <section className={style.previewSection}>
+                <div className={style.previewContainer}>
+                    <figure className={style.previewFigures} style={{ backgroundImage: `url(${preview1})` }}>
+                        <h3 className={style.previewHeaders}>{props.previewHeaderText1}</h3>
+                        <p>{props.previewText}</p>
+                        <button className={style.previewButtons}>{props.previewButton1}</button>
+                    </figure>
+                    <figure className={style.previewFigures} style={{ backgroundImage: `url(${preview2})` }}>
+                        <h3 className={style.previewHeaders}>{props.previewHeaderText2}</h3>
+                        <p>{props.previewText}</p>
+                        <button className={style.previewButtons}>{props.previewButton2}</button>
+                    </figure>
+                </div>
+            </section>
+        </>
+    )
+}
